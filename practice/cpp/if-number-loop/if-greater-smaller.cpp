6128e004ab42459f12460d9279ca345e4950b51d
pinkton <10314808+pinkton@users.noreply.github.com>
2026-01-03 20:07:14 +0000

    Implemented a loop and an input checker to ensure users (me) enters integers opposed to anything else. Uses a negative one (-1) as a quit, ability.

--- practice/cpp/if-number-loop/if-greater-smaller.cpp
+++ practice/cpp/if-number-loop/if-greater-smaller.cpp
@@ -2,25 +2,37 @@
 
 int main () {
     int number;
-    std::cout << "Please enter a number from 0 - 20:" << std::endl;
-    std::cin >> number;
+    while (true) {
+        std::cout << "Please enter a number from 0 - 20, or -1 to quit:" << std::endl;
+        std::cin >> number;
 
-    if (number > 10 && number <= 20) {
-        std::cout << "Number is greater than 10" << std::endl;
-    } 
-    else if (number < 10 && number >= 0)
-    {
-        std::cout << "Number is less than 10" << std::endl;
+        if (std::cin.fail()) {
+            std::cin.clear();  // Clear the error flags
+            std::cin.ignore(100, '\n');  // Discard up to 100 chars or until newline
+            std::cout << "Invalid input! Please enter a number." << std::endl;
+        }
+
+        if (number > 10 && number <= 20) 
+        {
+            std::cout << "Number is greater than 10" << std::endl;
+        } 
+        else if (number < 10 && number >= 0)
+        {
+            std::cout << "Number is less than 10" << std::endl;
+        }
+        else if (number == 10)
+        {
+            std::cout << "Number is exactly 10" << std::endl;
+        }
+        else if (number == -1)
+        {
+            std::cout << "Quitting!" << std::endl;
+            break;
+        }
+        else
+        {
+            std::cout << "Number is not within parameters, or is not a number, try again\n" << std::endl;
+        }
     }
-    else if (number == 10)
-    {
-        std::cout << "Number is exactly 10" << std::endl;
-    }
-    else
-    {
-        std::cout << "Number is not within parameters, or is not a number, try again" << std::endl;
-        main();
-    }
-    
     return 0;
 }
